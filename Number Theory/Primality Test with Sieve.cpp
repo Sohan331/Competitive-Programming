a95f4c80b86b37aa6740559a5bfa0f8d29828f12
Sohan331 <sohanuddin331@gmail.com>
2025-01-12 20:55:37 +0600

    a

---
+++ Number Theory/Primality Test with Sieve.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+
+const int N = 1e7;
+bool mark[N];
+
+using namespace std;
+
+int main()
+{
+   vector<int> prime;
+
+    // sieve;
+   for(int i = 2; i * i<= N ; i++){
+    if(mark[i] == false){
+        prime.push_back(i);
+       for(int j = i*i ; j <= N ; j+=i){
+         mark[j] = true;
+       }
+    }
+   }
+    //sieve
+
+   int t; cin >> t;
+   while(t--){
+
+      int n; cin >> n;
+      if(mark[n] == false) yes;
+      else no;
+   }
+
+}
