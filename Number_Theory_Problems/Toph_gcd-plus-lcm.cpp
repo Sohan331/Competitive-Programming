5c37279e2683e1fd421b31bf5b5363d05cfd7e05
Sohan331 <sohanuddin331@gmail.com>
2025-02-19 23:11:11 +0600

    solved

--- Number_Theory_Problems/Toph_gcd-plus-lcm.cpp
+++ Number_Theory_Problems/Toph_gcd-plus-lcm.cpp
@@ -1,7 +1,4 @@
-//starting in the name of Almighty ALLAH//
-//Bismillahir Rahmanir Rahim//
 #include <bits/stdc++.h>
-using namespace std;
 #define pb push_back
 #define ll long long
 #define yes cout << "YES" << "\n"
@@ -15,85 +12,96 @@ using namespace std;
 #define gap " "
 #define nl "\n"
 #define in insert
+#define wt while (t--)
+using namespace std;
 const ll M = 1e9 + 7;
-const int N = 1e8;
-vector<bool> mark(N + 1, false);
+const ll N = 1e7+1;
+vector<bool> mark(N,false);
 vector<ll> prime;
+void sieve();
+void precomp();
+vector<ll> pc(N + 1, 1);
+ll gcd(ll a, ll b);
+ll lcm(ll a, ll b);
 
+void solve()
+{
+    ll n,m;
+    cin>>n>>m;
+    gcd(n,m)+lcm(n,m)==n+m?cout<<"true"<<nl:cout<<"false"<<nl;
+
+}
+
+int main()
+{
+    fast();
+    sieve();
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        solve();
+    }
+
+}
 
 void sieve()
 {
     mark[0] = true;
     mark[1] = true;
-    for (int i = 4; i <=N; i += 2)
+    for (ll i = 4; i <= N; i += 2)
     {
         mark[i] = true;
     }
-    for (int i = 3; i * i <=N; i += 2)
+    for (ll i = 3; i * i <= N; i += 2)
     {
         if (!mark[i])
         {
-            for (int j = i * i; j < N; j += 2 * i)
+            for (ll j = i * i; j <= N; j += 2 * i)
             {
                 mark[j] = true;
             }
         }
     }
-
-    prime.push_back(0);
     prime.push_back(2);
 
     for (ll i = 3; i <=N; i += 2)
     {
         if (!mark[i])
         {
-           prime.push_back(i);
-
+            prime.push_back(i);
         }
-
     }
 }
 
-
-
-
-
-
-
-
-
-void solve()
+void precomp()
 {
-    ll n,m;
-    cin>>n>>m;
-    cout<<prime[m]-prime[n-1]<<nl;
+    ll cnt = 0;
+    for (ll i = 2; i <= N; i++)
+    {
+        if (!mark[i])
+        {
+            for (ll j = i; j <= N; j += i)
+            {
+                ll temp = j;
+                cnt = 0;
+                while (temp % i == 0)
+                {
+                    cnt++;
+                    temp /= i;
+                }
+                pc[j] *= cnt + 1;
+            }
+        }
+    }
 }
 
-
-
-
-int main()
+ll lcm(ll a, ll b)
 {
-    fast();
-    sieve();
-  
-
-    for(int i=2;i<prime.size();i++)
-    {
-        prime[i]+=prime[i-1];
-    }
-    int t;
-    cin>>t;
-    for(int i=1; i<=t; i++)
-    {
-        solve();
-    }
-
-
+    return (a / gcd(a, b))*b;
 }
-
-
-
-/////////////////////////////////////////////FUNCTIONS//////////////////////////////////////////////////////////////
-
-
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
