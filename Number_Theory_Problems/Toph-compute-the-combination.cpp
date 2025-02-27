d62c93947873beeadd2495542625e3c90c4e7acf
Sohan331 <sohanuddin331@gmail.com>
2025-02-27 22:08:21 +0600

    Solved

---
+++ Number_Theory_Problems/Toph-compute-the-combination.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define yes cout << "YES" << "\n"
+#define no cout << "NO" << "\n"
+#define nl "\n"
+#define fast() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+void solve()
+{
+    int n;
+    cin>>n;
+    if(n==0)cout<<1<<nl;
+    else cout<<0<<nl;
+}
+
+int main(){
+    fast();
+    int t;
+    cin>>t;
+    for (int i = 0; i <t; i++)
+    {
+        solve();
+    }
+            
+    }
+    
+  
